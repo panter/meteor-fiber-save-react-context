4d86f85ea9c5a71159444170e120974c96cc17e3
Marco <macrozone@gmail.com>
2018-10-17 17:11:30 +0200

    update

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'panter:fiber-save-react-context',
   version: '0.1.0',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'make the react 16 context-api for SSR using fibers',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/panter/meteor-fiber-save-react-context',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md',
